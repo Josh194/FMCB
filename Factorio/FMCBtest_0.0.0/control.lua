2486e5c30349c648072d2b5d0df9617ad0dfb535
Josh Nave <the4naves@gmail.com>
2020-02-06 18:11:06 -0700

    Update Tests

--- Factorio/FMCBtest_0.0.0/control.lua
+++ Factorio/FMCBtest_0.0.0/control.lua
@@ -1,6 +1,6 @@
 script.on_event({defines.events.on_tick},
    function (e)
-      if e.tick == 0 then
+      if e.tick % 120 == 0 then
          game.print("FMCB Tests has been correctly loaded")
       end
    end
