624779bc8f6633a5c0d7aeec6c8e3af8d3ec6709
Josh Nave <the4naves@gmail.com>
2020-12-19 19:21:47 -0700

    Safety Update

---
+++ Factorio/FMCB_0.0.1/data.lua
@@ -0,0 +1,2 @@
+require("prototypes.portalSend")
+require("prototypes.portalReceive")
--- Minecraft/FMCB/src/main/java/side/server/main_java_side_server_MemoryUtils.cpp
+++ Minecraft/FMCB/src/main/java/side/server/main_java_side_server_MemoryUtils.cpp
@@ -8,11 +8,11 @@ LPVOID pBuf;
 
 JNIEXPORT void JNICALL Java_main_java_side_server_MemoryUtils_createBuffer(JNIEnv* env, jclass thisClass) {
 	hMapFile = CreateFileMapping(
-		INVALID_HANDLE_VALUE,	// use paging file
-		NULL,			// default security
-  		PAGE_READWRITE,		// read/write access
-		0,			// maximum object size (high-order DWORD)
-		BUF_SIZE,		// maximum object size (low-order DWORD)
+		INVALID_HANDLE_VALUE,		// use paging file
+		NULL,						// default security
+  		PAGE_READWRITE,				// read/write access
+		0,							// maximum object size (high-order DWORD)
+		BUF_SIZE,					// maximum object size (low-order DWORD)
 		"Local\\FMCBuffer");		// name of mapping object
 }
 
@@ -23,11 +23,11 @@ JNIEXPORT void JNICALL Java_main_java_side_server_MemoryUtils_closeBuffer(JNIEnv
 }
 
 JNIEXPORT jobject JNICALL Java_main_java_side_server_MemoryUtils_mapBuffer(JNIEnv* env, jclass thisClass) {
-	pBuf = (LPTSTR) MapViewOfFile(hMapFile,	// handle to map object
+	pBuf = MapViewOfFile(hMapFile,	// handle to map object
 		FILE_MAP_ALL_ACCESS,		// read/write permission
-		0,				// file offset (high-order DWORD)
-		0,				// file offset (low-order DWORD)
-		BUF_SIZE);			// number of bytes to map
+		0,							// file offset (high-order DWORD)
+		0,							// file offset (low-order DWORD)
+		BUF_SIZE);					// number of bytes to map
 
 	return env -> NewDirectByteBuffer(pBuf, BUF_SIZE);
 }
