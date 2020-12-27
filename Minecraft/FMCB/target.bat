3ef1002d2b37544cf0a2f0a3bc195e83983d9274
Josh Nave <the4naves@gmail.com>
2020-12-26 18:20:44 -0700

    Full CMake build

---
+++ Minecraft/FMCB/target.bat
@@ -0,0 +1,5 @@
+@rem This file builds the core MC subsystem, and is run automatically via a CMake target
+
+gradlew build
+
+@rem Later, add functionality to copy files to correct directories, etc (could maybe be done by CMake?)
